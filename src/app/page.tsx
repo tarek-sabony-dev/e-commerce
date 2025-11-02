9ba1ac8fd2e1478fda2ecf7762713c94972e2c0d
tarek-sabony-dev <tareksabony@gmail.com>
2025-11-02 13:00:41 +0300

    upgrade to next 16 and testing cache mechanisims

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,38 @@
+import { unstable_cache } from "next/cache";
+import { GetCategories } from "./actions/actions";
 
-export default function CategoriesPage() {
+interface ShopPageProps {
+  searchParams: Promise<{
+    page?: string;
+    category?: string;
+  }>;
+}
+
+const getCachedUser = unstable_cache(
+  async () => {
+    const categories = await GetCategories();
+    return categories;
+  },
+  [],
+  {
+    tags: ["categories"],
+    revalidate: 60,
+  }
+)
+
+export default async function CategoriesPage() {
+  // const categories = await GetCategories();
+
+  const categories = await getCachedUser();
+  
   return (
-    <>
-     
-    </>
+    <div className="container mx-auto px-4 py-8">
+      <h1 className="text-3xl font-bold text-center mb-8">Categories</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {categories.map((category) => (
+          <div key={category.id}>{category.name}</div>
+        ))}
+      </div>
+    </div>
   )
 }
--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -16,7 +16,7 @@ export default async function ShopPage({ searchParams }: ShopPageProps) {
   const limit = 12;
   
   const productsData = await GetProducts(currentPage, limit, categoryId);
- 
+
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-8">Shop</h1>
