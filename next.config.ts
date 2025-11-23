3b0824a6aed8e4a0050130328fd6608ae834fb20
tarek-sabony-dev <tareksabony@gmail.com>
2025-11-23 12:01:04 +0300

    adding edge store for reading images

--- next.config.ts
+++ next.config.ts
@@ -5,9 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**', // This allows all image paths from this hostname
+        hostname: 'files.edgestore.dev',
       },
     ],
   },
